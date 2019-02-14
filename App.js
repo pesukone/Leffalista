0e2c3f06f58499e6ce697740507759b5a4d2aa64
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2019-02-14 21:18:50 +0200

    App now fetches movie data from api and list names of the movies

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import React, { Component, useState } from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component, useState } from 'react'
+import { Text, ScrollView } from 'react-native'
+import parser from 'fast-xml-parser'
 
 const App = () => {
-  const [text, setText] = useState("kukkuu")
+  const [movies, setMovies] = useState([])
 
-  return <View>
-    <Text>{text}</Text>
-  </View>
+  if (movies.length === 0) {
+    fetch('https://www.leffatykki.com/xml/ilta/tvssa/tv-today.xml')
+      .then(res => res.text())
+      .then(text => parser.parse(text))
+      .then(o => {
+        const movies = Object.values(o.today.movie)
+          .map(({ name, cover, broadcast }) => ({ name, cover, broadcast }))
+        //console.log(Object.keys(o.today.movie[0].broadcast))
+        
+        setMovies(movies)
+      })
+  }
+
+  return (
+    <ScrollView>
+      {movies.map(({ name, broadcast: { channel, date } }) => <Text key={channel + date}>{name}</Text>)}
+    </ScrollView>
+  )
 }
 
 export default App
