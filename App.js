c0b13ced4c07714f6d8d92b4362e382d19348c90
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2019-02-22 04:01:09 +0200

    movies are now separated to different pages based on date

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component, useState } from 'react'
-import { Text, ScrollView } from 'react-native'
+import { Text, View, ScrollView } from 'react-native'
+import Swiper from 'react-native-swiper'
 import parser from 'fast-xml-parser'
 
 const App = () => {
@@ -18,16 +19,39 @@ const App = () => {
       })
   }
 
-  return (
-    <ScrollView>
-      {movies.map(({ name, broadcast: { channel, date } }) => {
-        const d = new Date(date)
-        const dstring = `${d.getDate()}.${d.getMonth()}.${d.getFullYear()}`
+  console.log(separateOnDate(movies))
 
-        return <Text key={channel + date}>{`${name}, ${channel} ${dstring}`}</Text>
-      })}
-    </ScrollView>
+  return (
+    <Swiper loop={false} showsPagination={false}>
+      {separateOnDate(movies)
+        .map(mlist =>
+          <ScrollView key={mlist[0].broadcast.channel + mlist[0].broadcast.date}>
+            {mlist.map(({ name, broadcast: { channel, date } }) => {
+              const d = new Date(date)
+              const dstring = `${d.getDate()}.${d.getMonth()}.${d.getFullYear()}`
+
+              return <Text key={channel + date}>{`${name}, ${channel} ${dstring}`}</Text>
+            })}
+          </ScrollView>
+        )
+      }
+    </Swiper>
   )
 }
+
+const distinctDates = movies => 
+  Array.from(
+    new Set(
+      movies.map(({ broadcast: { date } }) =>
+        new Date(date).toDateString())
+    )
+  )
+
+const separateOnDate = movies =>
+  distinctDates(movies)
+    .map(dstring =>
+      movies.filter(({ broadcast: { date } }) =>
+        new Date(date).toDateString() === dstring)
+    )
 
 export default App
